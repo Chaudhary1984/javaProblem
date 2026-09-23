d180012b4a6a1cd1be6e211f83a5256aabb6964c
Manoj Chaudhary <kc.manoj84@gmail.com>
2026-09-23 01:38:43 -0500

    add java problem 001

---
+++ Problem001.java
@@ -0,0 +1,8 @@
+public class Problem001 {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 6;
+        int sum = a * b;
+        System.out.println("The sum of the two numbers is : "+ sum);
+    }
+}
